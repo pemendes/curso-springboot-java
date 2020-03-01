b22f03816abefa8b0a98e81cacbaf8417ccf83a2
Miqeas Pedo MENDES <miqeasmendes@gmail.com>
2020-03-01 01:38:49 +0100

    Project created

---
+++ src/test/java/com/cvmendes/cursospring/CursospringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cvmendes.cursospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
