d44197744e8b071b29a282c5259b1e88abdd2393
Miqeas Pedo MENDES <miqeasmendes@gmail.com>
2020-03-01 17:54:26 +0100

    Exception handling - delete

--- src/main/java/com/cvmendes/cursospring/services/UserService.java
+++ src/main/java/com/cvmendes/cursospring/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.cvmendes.cursospring.entities.User;
 import com.cvmendes.cursospring.repositories.UserRepository;
+import com.cvmendes.cursospring.resources.exceptions.DatabaseException;
 import com.cvmendes.cursospring.resources.exceptions.ResourceNotFoundException;
 
 // anotation pour enregistrer UserService 
@@ -32,7 +35,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
