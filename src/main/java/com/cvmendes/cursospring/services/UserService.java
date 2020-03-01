75844b69af83dc542a2f70588b2f15aeb621cc0b
Miqeas Pedo MENDES <miqeasmendes@gmail.com>
2020-03-01 13:42:45 +0100

    Service layer, component registration

---
+++ src/main/java/com/cvmendes/cursospring/services/UserService.java
@@ -0,0 +1,28 @@
+package com.cvmendes.cursospring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cvmendes.cursospring.entities.User;
+import com.cvmendes.cursospring.repositories.UserRepository;
+
+// anotation pour enregistrer UserService 
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
