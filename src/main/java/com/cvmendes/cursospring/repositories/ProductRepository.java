8201812db2d290349c2a85b39d4632238a0391e3
Miqeas Pedo MENDES <miqeasmendes@gmail.com>
2020-03-01 16:07:49 +0100

    Product

---
+++ src/main/java/com/cvmendes/cursospring/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.cvmendes.cursospring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cvmendes.cursospring.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
---
+++ src/main/java/com/cvmendes/cursospring/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.cvmendes.cursospring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cvmendes.cursospring.entities.Product;
+import com.cvmendes.cursospring.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repository;
+
+	public List<Product> findAll() {
+		return repository.findAll();
+	}
+
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}
