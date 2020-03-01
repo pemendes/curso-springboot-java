339df425642b4e62e062a35e9296b3ae7222568c
Miqeas Pedo MENDES <miqeasmendes@gmail.com>
2020-03-01 17:18:31 +0100

    Payment, one-to-one association

--- src/main/java/com/cvmendes/cursospring/config/TestConfig.java
+++ src/main/java/com/cvmendes/cursospring/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.cvmendes.cursospring.entities.Category;
 import com.cvmendes.cursospring.entities.Order;
 import com.cvmendes.cursospring.entities.OrderItem;
+import com.cvmendes.cursospring.entities.Payment;
 import com.cvmendes.cursospring.entities.Product;
 import com.cvmendes.cursospring.entities.User;
 import com.cvmendes.cursospring.entities.enums.OrderStatus;
@@ -80,5 +81,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));	
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+
+		orderRepository.save(o1);
 	}
 }
