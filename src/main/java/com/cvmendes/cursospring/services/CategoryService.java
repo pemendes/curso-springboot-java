1e2aa00b96941a75e03af93353cea0cb8931e19c
Miqeas Pedo MENDES <miqeasmendes@gmail.com>
2020-03-01 15:21:00 +0100

    Category

---
+++ src/main/java/com/cvmendes/cursospring/services/CategoryService.java
@@ -0,0 +1,28 @@
+package com.cvmendes.cursospring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cvmendes.cursospring.entities.Category;
+import com.cvmendes.cursospring.repositories.CategoryRepository;
+
+// anotation pour enregistrer CategoryService 
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}
